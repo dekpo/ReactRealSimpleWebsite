7a3ec362c3a47bc27f6bd5d9f00ef863c51ecd22
Dekpo <dekpo@me.com>
2021-07-21 16:53:31 +0200

    Added an image file to HomePage

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,6 +1,7 @@
 const PageHome = () => (
     <section>
         <h3>Home</h3>
+        <img src={require("../assets/images/lake-1681485_640.jpeg").default} alt="Montagne Lac Chalet" className="img-fluid" /> 
         <p>lorem ipsum dolor es truc muche</p>
     </section>
 )
