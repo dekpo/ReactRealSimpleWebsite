1663fc45f536b54f99f53739218dd9a40e2156e8
Dekpo <dekpo@me.com>
2021-07-21 17:08:30 +0200

    Added a fetch method and some hooks to handle API get

--- src/pages/PagePorfolio.js
+++ src/pages/PagePorfolio.js
@@ -1,8 +1,21 @@
-const PagePortfolio = () => (
-    <section>
-        <h3>Portfolio</h3>
-        <p>lorem ipsum dolor es truc muche</p>
-    </section>
-)
+import React, { useState, useEffect } from 'react';
+
+const PagePortfolio = () => {
+    const [gallery,setGallery] = useState([]);
+
+    useEffect( () => {
+        fetch("https://picsum.photos/v2/list?limit=4")
+        .then( res => res.json() )
+        .then( json => console.log(json) )
+    },[])
+
+    return(
+        <section>
+            <h3>Portfolio</h3>
+            <p>lorem ipsum dolor es truc muche</p>
+        </section>
+    )
+}
+
 
 export default PagePortfolio
